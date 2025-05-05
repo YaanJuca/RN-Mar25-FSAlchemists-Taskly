1b13ee4cf9dfd143bf58affe4de3a79271edb621
ElvisReis2k <elvis_vante@hotmail.com>
2025-05-05 16:46:17 -0300

    Feat: New homepage

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import styles from './style';
 
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
       
